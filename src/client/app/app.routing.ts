b651305336693cbb269043214fa66bffeba9a5eb
NSS22 <sergey.negoda1186@gmail.com>
2018-02-08 17:09:47 +0200

    Created components: UserSearchComponent, UserFilterComponent, SidebarMenuComponent

--- src/client/app/app.routing.ts
+++ src/client/app/app.routing.ts
@@ -1,18 +1,10 @@
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import {UserSearchComponent} from './components/user-search/user-search.component';
 
 export const routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'about'
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
-  }
+  { path: '', pathMatch: 'full', redirectTo: 'about' },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'search', component: UserSearchComponent }
 ];
