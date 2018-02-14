de932a3d92a793e1dd105c4f93fe0df2fd9f6031
Tanya <marych.tanya@gmail.com>
2018-02-14 16:35:47 +0200

    adaptive find

--- src/client/app/components/home/home.component.ts
+++ src/client/app/components/home/home.component.ts
@@ -10,7 +10,22 @@ export class HomeComponent implements OnInit {
     {
       image:'https://maxcdn.icons8.com/app/uploads/2017/05/Like-500.png',
       title:'Lorem impum',
+      text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer '
+    },
+    {
+      image:'https://maxcdn.icons8.com/app/uploads/2017/05/Like-500.png',
+      title:'Lorem impum',
       text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    },
+    {
+      image:'https://maxcdn.icons8.com/app/uploads/2017/05/Like-500.png',
+      title:'Lorem impum',
+      text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    },
+    {
+      image:'https://maxcdn.icons8.com/app/uploads/2017/05/Like-500.png',
+      title:'Lorem impum',
+      text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer '
     },
     {
       image:'https://maxcdn.icons8.com/app/uploads/2017/05/Like-500.png',
