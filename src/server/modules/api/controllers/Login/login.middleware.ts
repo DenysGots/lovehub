229be1c7bc30551c2c99f53c3b4ad13c4cb1bf91
Alexey Leybichenko <leibalex91@gmail.com>
2018-02-22 15:39:22 +0200

    add small changes

--- src/server/modules/api/controllers/Login/login.middleware.ts
+++ src/server/modules/api/controllers/Login/login.middleware.ts
@@ -6,13 +6,10 @@ export class LoginMiddleware implements NestMiddleware {
   constructor(@Inject('LoginValidateService') private logValidator: LoginValidateService) {}
   resolve(...ags: any[]): ExpressMiddleware {
     return (req, res, next) => {
-      console.log(req.body);
         const {email, password} = req.body;
         if (!this.logValidator.validateEmail(email) || !this.logValidator.validatePassword(password)) {
-          console.log('no valid');
           res.send({});
         } else {
-          console.log('valid');
           next();
         }
     };
