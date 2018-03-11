a0dace17f801d9f46c6b5edf87456bc3df460969
DenysGots <gotsdo@gmail.com>
2018-03-11 22:26:15 +0200

    Notifications templates, styles and client logic added

--- src/client/app/components/notifications/notifications.component.ts
+++ src/client/app/components/notifications/notifications.component.ts
@@ -1,18 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 import { NotificationsService } from '../../services/notifications.service';
+
+interface Message {
+  user: string;
+  isHidden: boolean;
+  isShifted: boolean;
+}
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: [
+    './notifications.component.scss'
+  ]
 })
-export class NotificationsComponent implements OnInit {
+export class NotificationsComponent implements OnInit, OnDestroy {
+  messages = [];
+  connection;
+  notificationsList = {
+    isHidden: true,
+    minHeight: '0'
+  };
 
   constructor(private notificationsService: NotificationsService) {
   }
 
   ngOnInit() {
+    this.connection = this.notificationsService.getMessages().subscribe(message => {
+      this.handleMessages(message);
+    });
+  }
+
+  ngOnDestroy() {
+    this.connection.unsubscribe();
+  }
+
+  sendLike(message): void {
+    this.notificationsService.sendMessage(message);
+  }
+
+  closeNotificationsList(): void {
+    this.messages = [];
+    this.notificationsList.isHidden = true;
+  }
+
+  handleMessages(message): void {
+    const messages = this.messages;
+
+    let currentMessage = {} as Message;
+
+    const randomSequence = (Math.round(Math.random() * 1000000)).toString();      // TODO: delete
+
+    messages.push(<Message>{
+      user: randomSequence,     // TODO: change "rand" to "message"
+      isHidden: true,
+      isShifted: true
+    });
+
+    for (let i = 0; i < messages.length; i += 1) {
+      if (messages[i]['user'] === randomSequence) {
+        currentMessage = messages[i];
+        break;
+      }
+    }
+
+    // if (this.notificationsList.isHidden) {
+    //   setTimeout(() => {
+    //     this.notificationsList.isHidden = false;
+    //   }, 0);
+    // }
+
+    if (this.notificationsList.isHidden) {
+      this.notificationsList.isHidden = false;
+    }
+
+    setTimeout(() => {
+      currentMessage.isHidden = false;
+    }, 500);
+
+    setTimeout(() => {
+      currentMessage.isShifted = false;
+    }, 1000);
+
+    setTimeout(() => {
+      currentMessage.isShifted = true;
+    }, 3500);
+
+    setTimeout(() => {
+      currentMessage.isHidden = true;
+    }, 4000);
+
+    setTimeout(() => {
+      messages.splice(messages.indexOf(currentMessage), 1);
+
+      if (messages.length === 0) {
+        this.notificationsList.isHidden = true;
+      }
+    }, 4500);
   }
 
 }
--- src/client/app/services/notifications.service.ts
+++ src/client/app/services/notifications.service.ts
@@ -1,18 +1,48 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  })
-};
+import * as io from 'socket.io-client';
 
 @Injectable()
 export class NotificationsService {
+  private serverURL = '';
+  private socket = io(this.serverURL);
 
-  constructor(private http: HttpClient) {
+  constructor() {
+    this.socket.on('receive-notification', (data) => {
+      console.log(data);
+    });
+
+    this.socket.on('WORKING', (data) => {
+      console.log(data);
+    });
+  }
+
+  sendMessage(message) {
+    this.socket.emit('send-notification', message);
+    console.log(this.socket);
+  }
+
+  getMessages() {
+    const observable = new Observable(observer => {
+      this.socket.on('receive-notification', (data) => {
+        console.log(data);
+
+        observer.next(data);
+      });
+
+      this.socket.on('WORKING', (data) => {
+        console.log(data);
+      });
+
+      return () => {
+        this.socket.disconnect();
+      };
+    });
+
+    return observable;
   }
 
 }
