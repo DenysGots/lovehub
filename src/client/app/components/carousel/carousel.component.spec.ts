3e87c928a67149d74752063a6d27d476b0576414
Marina Bodak <bodak.marina.ev@gmail.com>
2018-03-21 12:55:44 +0200

    new photo-carousel

--- src/client/app/components/carousel/carousel.component.spec.ts
+++ src/client/app/components/carousel/carousel.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PhotoSliderComponent } from './photo-slider.component';
+import { CarouselComponent } from './carousel.component';
 
-describe('PhotoSliderComponent', () => {
-  let component: PhotoSliderComponent;
-  let fixture: ComponentFixture<PhotoSliderComponent>;
+describe('CarouselComponent', () => {
+  let component: CarouselComponent;
+  let fixture: ComponentFixture<CarouselComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PhotoSliderComponent ]
+      declarations: [ CarouselComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PhotoSliderComponent);
+    fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
