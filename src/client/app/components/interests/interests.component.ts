2b844b53ba94e4ab71100a4a339558c29e5b4c2d
DenysGots <gotsdo@gmail.com>
2018-03-21 23:42:17 +0200

    Interests component work in progress

--- src/client/app/components/interests/interests.component.ts
+++ src/client/app/components/interests/interests.component.ts
@@ -13,7 +13,13 @@ import { InterestsService } from '../../services/interests.service';
 })
 export class InterestsComponent implements OnInit {
   public editMode = false;
-  public userTypingNewInterest = false;
+  public inputFieldFocus = false;
+  public interests: string[] = ['reading', 'football', 'tv watching', 'skiing', 'snowboarding', 'skydiving', 'skating', 'swimming', 'traveling', 'working', 'etc.'];
+  public typedInterest: string;
+  public unsavedAddedInterests: string[] = [];
+  public unsavedDeletedInterests: string[] = [];
+  public hintsList: string[] = [];
+  public hintsToShowList: string[] = ['reading', 'football', 'tv watching'];
 
   constructor(private interestsService: InterestsService) {
   }
@@ -21,16 +27,67 @@ export class InterestsComponent implements OnInit {
   ngOnInit() {
   }
 
-  // Add and save interest from input field
+  // Add interest from input field
   addInterest() {
+    this.unsavedAddedInterests.push(this.typedInterest);
+    this.typedInterest = '';
+  }
+
+  // Add interest from drop down hints
+  addInterestFromHint(interest) {
+    this.unsavedAddedInterests.push(interest);
+    this.typedInterest = '';
   }
 
   // Delete interest from interest list in user profile back at DB
-  deleteInterest() {
+  deleteInterest(interest) {
+    this.unsavedDeletedInterests.push(interest);
+
+    console.log(this.unsavedDeletedInterests);
+    console.log(this.unsavedDeletedInterests.indexOf(interest));
   }
 
-  // Add and save interest from drop down interests list (hints)
-  addInterestFromHint() {
+  // Save changes and exit editing mode
+  saveChanges() {
+    const interests = this.interests;
+    const addedInterests = this.unsavedAddedInterests;
+    const deletedInterests = this.unsavedDeletedInterests;
+    const addedInterestsLength = this.unsavedAddedInterests.length;
+    const deletedInterestsLength = this.unsavedDeletedInterests.length;
+
+    if (deletedInterestsLength) {
+      for (let i = 0; i < deletedInterestsLength; i += 1) {
+        if (interests.indexOf(deletedInterests[i])) {
+          interests.splice(interests.indexOf(deletedInterests[i]), 1);
+        }
+      }
+    }
+
+    if (addedInterestsLength) {
+      this.interests = [...interests, ...addedInterests];
+    }
+
+    this.editMode = false;
+  }
+
+  // Toggle editing mode and undo changes on exit
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+    this.unsavedAddedInterests = [];
+    this.unsavedDeletedInterests = [];
+  }
+
+  // Show hints on input filed in focus
+  onInputFocus() {
+    this.inputFieldFocus = true;
+
+    console.log(this.inputFieldFocus);
+  }
+
+  onInputBlur() {
+    this.inputFieldFocus = false;
+
+    console.log(this.inputFieldFocus);
   }
 
 }
