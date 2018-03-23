a7abb2446b543c6648308c5a5c0d1de9093c8a56
Denys Gots <gotsdo@gmail.com>
2018-03-23 15:02:45 +0000

    Update app.module.ts

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -3,7 +3,8 @@ import { Module } from '@nestjs/common';
 import { ApiModule } from './modules/api/api.module';
 import { StaticModule } from './modules/static/static.module';
 import { HomepageModule } from './modules/homepage/homepage.module';
-import {MatchingModule} from './modules/matching/matching.module';
+import { MatchingModule } from './modules/matching/matching.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { NotificationsModule } from './modules/api/notifications/notifications.module';
 import { AdministratorModule } from './modules/api/administrator/administrator.module';
 
@@ -12,6 +13,8 @@ import { AdministratorModule } from './modules/api/administrator/administrator.m
     ApiModule,
     HomepageModule,
     MatchingModule,
+    AuthModule,
+    StaticModule,
     NotificationsModule,
     AdministratorModule,
     StaticModule // Must be the last one !
