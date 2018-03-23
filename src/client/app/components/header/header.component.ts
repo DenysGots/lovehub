49e24d655cb175d18e76a6de56bf25aabcad3abd
Denys Gots <gotsdo@gmail.com>
2018-03-23 14:12:32 +0000

    Update header.component.ts

--- src/client/app/components/header/header.component.ts
+++ src/client/app/components/header/header.component.ts
@@ -19,7 +19,7 @@ export class HeaderComponent implements OnInit {
   constructor(
     private navService: NavigationService,
     private windowService: WindowService,
-    private _router: Router
+    _router: Router
   ) {
     this.router = _router;
   }
