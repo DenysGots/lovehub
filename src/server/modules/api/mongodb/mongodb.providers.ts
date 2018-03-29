85db306d5c0be0392e12ffc97dac6da8520a6638
Alexey Leybichenko <leibalex91@gmail.com>
2018-03-29 17:02:26 +0300

    final changes

--- src/server/modules/api/mongodb/mongodb.providers.ts
+++ src/server/modules/api/mongodb/mongodb.providers.ts
@@ -5,7 +5,7 @@ export const mongodbProviders = [
     provide: 'MongodbConnectionToken',
     useFactory: async () => {
       (mongoose as any).Promise = global.Promise;
-      return await mongoose.connect('mongodb://lovehub:pass@localhost:27017/lovehub');
+      return await mongoose.connect('mongodb://localhost:27017/lovehub');
     },
   },
 ];
