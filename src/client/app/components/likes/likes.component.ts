13d41885e31bd9562dc99bec2403c802e8cbb971
Marina Bodak <bodak.marina.ev@gmail.com>
2018-04-04 17:01:29 +0300

    template done

--- src/client/app/components/likes/likes.component.ts
+++ src/client/app/components/likes/likes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../../services/photos.service';
+import * as jwt_decode from 'jwt-decode';
+import { Photo } from '../../models/photo';
+import { LikesService } from '../../services/likes.service';
+
 
 @Component({
   selector: 'app-likes',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikesComponent implements OnInit {
 
-  constructor() { }
+  userId: number;
+  photos: Photo[] = [ {userId: 0, _id: '', base64: '', avatar: false, name: ''} ];
+
+  constructor(private photosService: PhotosService, private likesService: LikesService) { }
 
   ngOnInit() {
+    this.userId = parseInt(jwt_decode(localStorage.getItem('jwt_token')).id, 10);
+    this.photosService.getPhotos(this.userId)
+      .subscribe(items => {
+        this.photos = items;
+      });
   }
+
+
 
 }
